6f1a3b29b40e6271f00fe6d4c6604931e19af49a
Dmitry Nazarov <dnazarov71317@gmail.com>
2023-11-14 16:46:34 +0400

    "Добавил библиотеку faker к тестовым данным. Добавил метод для поиска города в зависимости от штата. Добавил метод для даты из календаря в зависимости от месяца"

--- src/test/java/data/DataStudentRegistrationForm.java
+++ src/test/java/data/DataStudentRegistrationForm.java
@@ -1,26 +1,112 @@
 package data;
 
 
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class DataStudentRegistrationForm {
-    public String firstName = "Кот",
-            lastName = "Матроскин",
-            email = "bestkotik@gmail.com",
-            gender = "Male",
-            mobileNumber = "9999999999",
-            day = "30",
-            mouth = "June",
-            year = "2001",
-            subjectValue = "E",
-            pictureName = "photo_cat.jpg",
-            hobbieValue = "Sports",
-            address = "Молочный край, город Мяу, улица Пушистая дом 7",
-            stateButton = "#state",
-            stateName = "NCR",
-            cityButton = "#city",
-            cityName = "Delhi";
 
+
+    Faker faker = new Faker(new Locale("ru"));
+    Faker emailfaker = new Faker(new Locale("en-GB"));
+    RandomValue randomValue = new RandomValue();
+
+    String[] stateMassive = {"NCR", "Uttar Pradesh", "Haryana", "Rajasthan"};
     public String[] keyMassive = {"Student Name", "Student Email", "Gender", "Mobile", "Date of Birth",
-            "Subjects", "Hobbies", "Picture", "Address", "State and City"};
+                                "Subjects", "Hobbies", "Picture", "Address", "State and City"};
 
+    public String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = emailfaker.internet().emailAddress(),
+            gender = faker.options().option("Male", "Female", "Other"),
+            mobileNumber = faker.phoneNumber().subscriberNumber(10),
+            mouth = faker.options().option("January", "February", "March", "April", "May", "June",
+                                        "July", "August", "September", "October", "November", "December"),
+            day = randomValue.getDayDependingOnMouth(mouth),
+            year = randomValue.getRandomIntToString(1990, 2005),
+            subjectValue = faker.options().option("A", "B", "C", "D", "E", "J",
+                                                    "K", "O", "P", "Q", "S", "U"),
+            pictureName = "photo_cat.jpg",
+            hobbieValue = faker.options().option("Sports", "Reading", "Music"),
+            address = faker.address().streetAddress(),
+            stateButton = "#state",
+            stateName = randomValue.getRandomItemFromArray(stateMassive),
+            cityButton = "#city",
+            cityName = randomValue.getCityDependingOnState(stateName);
+
+    class RandomValue {
+        public String getRandomItemFromArray(String[] array) {
+            int index = getRandomInt(0, array.length - 1);
+            return array[index];
+        }
+
+        public int getRandomInt(int min, int max) {
+
+            return ThreadLocalRandom.current().nextInt(min, max + 1);
+        }
+
+        public String getRandomIntToString(int min, int max) {
+            return String.valueOf(ThreadLocalRandom.current().nextInt(min, max + 1));
+        }
+
+        public String getCityDependingOnState(String stateName) {
+            String city = "XXX";
+            if (stateName.equals("NCR")) {
+                String[] cityNameMassive = {"Delhi", "Gurgaon", "Noida"};
+                String city1 = getRandomItemFromArray(cityNameMassive);
+                city = city1;
+            } else if (stateName.equals("Uttar Pradesh")) {
+                String[] cityNameMassive = {"Agra", "Lucknow", "Merrut"};
+                String city2 = getRandomItemFromArray(cityNameMassive);
+                city = city2;
+            } else if (stateName.equals("Haryana")) {
+                String[] cityNameMassive = {"Karnal", "Panipat"};
+                String city3 = getRandomItemFromArray(cityNameMassive);
+                city = city3;
+            } else if (stateName.equals("Rajasthan")) {
+                String[] cityNameMassive = {"Jaipur", "Jaiselmer"};
+                String city4 = getRandomItemFromArray(cityNameMassive);
+                city = city4;
+            }
+            return city;
+        }
+
+        public String getDayDependingOnMouth(String mouth) {
+            String day = "";
+            switch (mouth) {
+                case "January":
+                case "March":
+                case "May":
+                case "July":
+                case "August":
+                case "December":
+                    day = faker.options().option("01", "02", "03", "04", "05", "06", "07", "08", "09",
+                            "10", "11", "12", "13", "14", "15", "16", "17", "18",
+                            "19", "20", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31");
+                    break;
+                case "April":
+                case "June":
+                case "September":
+                case "November":
+                    day = faker.options().option("01", "02", "03", "04", "05", "06", "07", "08", "09",
+                            "10", "11", "12", "13", "14", "15", "16", "17", "18",
+                            "19", "20", "22", "23", "24", "25", "26", "27", "28", "29", "30");
+                    break;
+                case "February":
+                    day = faker.options().option("01", "02", "03", "04", "05", "06", "07", "08", "09",
+                            "10", "11", "12", "13", "14", "15", "16", "17", "18",
+                            "19", "20", "22", "23", "24", "25", "26", "27", "28");
+                    break;
+            }
+            return day;
+        }
+
+    }
 
 }
+
+
+
+
